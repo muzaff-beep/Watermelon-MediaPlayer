08c6a36c86482b6cf861cd66b78119cdfc138675
So Muzaffari <so.muzaff@gmail.com>
2026-06-09 05:05:26 +0330

    Update PlaylistRepository.kt

--- common-interfaces/src/main/kotlin/com/watermelon/common/repository/PlaylistRepository.kt
+++ common-interfaces/src/main/kotlin/com/watermelon/common/repository/PlaylistRepository.kt
@@ -1,11 +1,37 @@
 package com.watermelon.common.repository
 
+import com.watermelon.common.model.MediaItem
 import com.watermelon.common.model.Playlist
 import kotlinx.coroutines.flow.Flow
 
 interface PlaylistRepository {
-    fun observePlaylists(): Flow<List<Playlist>>
-    suspend fun createPlaylist(name: String): Playlist
-    suspend fun addToPlaylist(playlistId: String, mediaUri: String)
-    suspend fun removeFromPlaylist(playlistId: String, mediaUri: String)
+    /** All playlists in display order: Recently Added, Favourites, then user playlists. */
+    fun observeAll(): Flow<List<Playlist>>
+
+    /** Videos inside a specific playlist. */
+    fun observeVideos(playlistId: String): Flow<List<MediaItem>>
+
+    /** Add a video to Favourites. */
+    suspend fun addToFavourites(uri: String)
+
+    /** Remove a video from Favourites. */
+    suspend fun removeFromFavourites(uri: String)
+
+    /** Check if a video is in Favourites. */
+    suspend fun isFavourite(uri: String): Boolean
+
+    /** Create a new user playlist. Returns the new playlist id. */
+    suspend fun createPlaylist(name: String): String
+
+    /** Delete a user playlist. System playlists cannot be deleted. */
+    suspend fun deletePlaylist(playlistId: String)
+
+    /** Rename a user playlist. */
+    suspend fun renamePlaylist(playlistId: String, newName: String)
+
+    /** Add a video to a user playlist. */
+    suspend fun addToPlaylist(playlistId: String, uri: String)
+
+    /** Remove a video from a user playlist. */
+    suspend fun removeFromPlaylist(playlistId: String, uri: String)
 }
